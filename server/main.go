f89ff541f75d46b9196ecbbdb2c92cc36a3137c9
xhd2015 <xhd2015@gmail.com>
2024-10-03 19:51:53 +0800

    update server

--- server/main.go
+++ server/main.go
@@ -20,35 +20,40 @@ func main() {
 
 func handle() error {
 	staticDir := "./dist"
-	fs := http.FileServer(http.Dir(staticDir))
 	var serveFiles http.HandlerFunc = func(w http.ResponseWriter, r *http.Request) {
 		log.Printf("request: Method=%s, Host=%s, RemoteAddr=%s, RequestURI=%s, Header=%v", r.Method, r.Host, r.RemoteAddr, r.RequestURI, r.Header)
 
 		file := filepath.Join(staticDir, strings.ReplaceAll(r.RequestURI, "/", string(filepath.Separator)))
 		fsInfo, statErr := os.Stat(file)
 		if statErr != nil {
-			if !errors.Is(statErr, os.ErrNotExist) {
-				http.Error(w, statErr.Error(), 500)
-				return
+			if errors.Is(statErr, os.ErrNotExist) {
+				file = filepath.Join(staticDir, "index.html")
+				fsInfo, statErr = os.Stat(file)
 			}
-		} else if !fsInfo.IsDir() {
-			// is file
-			osFile, err := os.Open(file)
-			if err != nil {
-				http.Error(w, err.Error(), 500)
-				return
-			}
-			defer osFile.Close()
-			http.ServeContent(w, r, file, fsInfo.ModTime(), osFile)
+		} else if fsInfo.IsDir() {
+			file = filepath.Join(file, "index.html")
+			fsInfo, statErr = os.Stat(file)
+		}
+
+		if statErr != nil {
+			http.Error(w, statErr.Error(), http.StatusInternalServerError)
 			return
 		}
-		fs.ServeHTTP(w, r)
+
+		// is file
+		osFile, err := os.Open(file)
+		if err != nil {
+			http.Error(w, err.Error(), 500)
+			return
+		}
+		defer osFile.Close()
+		http.ServeContent(w, r, file, fsInfo.ModTime(), osFile)
 	}
 	http.Handle("/", serveFiles)
 
 	port := os.Getenv("PORT")
 	if port == "" {
-		port = "8080"
+		port = "8087"
 	}
 	log.Printf("Listening on :%s...", port)
 	return http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
